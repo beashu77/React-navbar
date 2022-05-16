f77759ce82f0152e3c444c8b3a6f1499ab4c688c
Ashwini Bhoskar <ashwinibhoskar5111@gmail.com>
2022-05-17 03:39:10 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import './navbar.css';
+import {Logo,Links,Btn} from './components/navbar';
 
 function App() {
   return (
     <div className="navbar">
-     <div id="one">LOGOBAKERY</div>
-     <div id="two">
-       <h4>Services</h4>
-       <h4>Projects</h4>
-       <h4>About</h4>
-     </div>
-     <div id="three">
-       <button id="btn">Contact</button>
-     </div>
+
+      <Logo> </Logo>
+      <Links></Links>
+      <Btn></Btn>
+  
+      
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+
+const Logo=()=>{
+    return(
+        <div id="Logo">LOGOBAKERY</div>
+    )
+}
+
+
+
+const Links=()=>{
+    return(
+        <div id="Links">
+            <p>Services</p>
+            <p>Products</p>
+            <p>About</p>
+        </div>
+    )
+}
+
+
+const Btn=()=>{
+    return(
+        <button id="Btn">Contact</button>
+    )
+}
+export {Logo,Links,Btn};
